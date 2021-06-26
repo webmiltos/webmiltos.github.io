52692c89c73ef1e9ee6825332a5f95b117d7e01d
Milt <milkats23@gmail.com>
2021-06-26 15:12:03 +0300

    milt

--- assets/includes/copyright.php
+++
@@ -1,2 +0,0 @@
-<h4>Code Dynamic Websites with PHP</h4>
-<h5>Copyright &copy;<?php echo date('Y');?> <a href="http://bradhussey.ca/" target="_blank">Brad Hussey</a> of <a href="http://codecollege.ca" target="_blank">CodeCollege.ca</a></h5>
